295c456f92302256e27ba31525572f2de77c6f38
MarwanAhmed2025 <marwen775459683@gmail.com>
2026-08-10 12:16:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from telethon import TelegramClient, events
-API_ID=int(os.getenv('API_ID'))
-API_HASH=os.getenv('API_HASH')
-BOT_TOKEN=os.getenv('BOT_TOKEN')
+API_ID=int("35469979")
+API_HASH="896f98c871cafe2f6d064bbcbdd4930a"
+BOT_TOKEN="8764144018:AAEafolKUYuUGmuH3mQaHX6Tc77GESCr9HA"
 client=TelegramClient('s',API_ID,API_HASH).start(bot_token=BOT_TOKEN)
 @client.on(events.NewMessage(pattern='/start'))
 async def s(e):await e.reply("ok")
